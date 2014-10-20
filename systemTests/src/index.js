c8627ee12bf4525be9348e131843fe85edf0af7e
yohana <yohana@yohanakhoury.com>
2014-10-20 13:10:33 +0300

    tests - re-enable AWS tests

--- systemTests/src/index.js
+++ systemTests/src/index.js
@@ -414,7 +414,7 @@ function stepCheckErrorBox(expectedErrorMessage, callback) {
     ])
 }
 
-xdescribe('snippet tests', function () {
+describe('snippet tests', function () {
     // AWS tests
 
     describe("AWS tests", function () {
