85dc7c0bff838ef8d9afb0d0eb595ce499b2fdfd
yohana <yohana@yohanakhoury.com>
2014-10-20 13:09:35 +0300

    tests-utils - updated env-paths

--- systemTests/src/utils/terminateSoftlayerMachines.js
+++ systemTests/src/utils/terminateSoftlayerMachines.js
@@ -8,7 +8,7 @@ var path = require('path');
 var logger = require('log4js').getLogger('terminateMachines');
 
 var softlayerJsonEnv = process.env.SOFTLAYER_JSON;
-var confPath = ( softlayerJsonEnv && path.resolve(__dirname + '/../../../', softlayerJsonEnv) ) || path.resolve(__dirname, '../../conf/dev/softlayer.json');
+var confPath = ( softlayerJsonEnv && path.resolve(__dirname + '../../../', softlayerJsonEnv) ) || path.resolve(__dirname, '../../conf/dev/softlayer.json');
 var conf = require(confPath);
 
 /**
