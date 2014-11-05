5e2fcd0e0a65213747cfa25ff8a59e220e6b464d
guym <guym@gigaspaces.com>
2014-11-05 20:32:40 +0200

    fix some tests.

--- test/spec/controllers/SoftlayerPackageItems.js
+++ test/spec/controllers/SoftlayerPackageItems.js
@@ -16,7 +16,13 @@ describe('Controller: SoftlayerPackageItemsCtrl', function () {
         });
     }));
 
-    it('should attach a list of awesomeThings to the scope', function () {
-        expect(scope.awesomeThings.length).toBe(3);
-    });
+    it('detect softlayerItems is on window and put it on scope', inject(function ( $timeout) {
+        window.softlayerItems = 'this is softlayer items';
+        waits(50);
+        $timeout.flush();
+        runs( function(){
+            expect(scope.softlayerItems).toBe('this is softlayer items');
+        })
+
+    }));
 });
