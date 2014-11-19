fad09ed0fac60f7c26f0da1baa76db434274e4a2
LironHazan <liron@gigaspaces.com>
2014-11-19 18:58:24 +0200

    liron: added yopmail api

---
+++ systemTests/src/components/external/yopmail.js
@@ -0,0 +1,98 @@
+/**
+ * Created by liron on 11/18/14.
+ */
+
+'use strict';
+
+var components = require('../index');
+var logger = require('log4js').getLogger('testAws');
+var q = require('q');
+var globalSteps = require('./../../utils/globalTestSteps');
+var config = require('../config/index');
+var driver = require('../driver/index');
+var By = require('selenium-webdriver').By;
+var assert = require('assert');
+var async = require('async');
+
+
+var SECOND = 1000;
+
+var webdriver;
+
+
+exports.loginToYopMailInbox =  function(webdriver, callback){
+
+    logger.info('loginToYopmail');
+    var retval = false;
+
+    webdriver.get("http://www.yopmail.com/en/?login=cloudifywidgettest123").then(function () {
+                logger.info('waiting for frame to load');
+                var x = false;
+                webdriver.wait(function () {
+                   setTimeout(function () {
+                       x = true;
+                  }, 5000);
+                 return x;
+             }, 7000);
+
+           logger.info('switching to frame');
+           webdriver.switchTo().frame('ifmail');
+           retval = true;
+
+            callback('err', retval);
+
+        });
+
+    }
+
+
+        exports.getLatestMessageDayAsString = function (webdriver, callback) {
+
+            var mailReceivedInDay;
+
+            webdriver.findElement(By.css('#mailhaut > div:nth-child(4)')).getText().then( function (text) {
+
+                var currDate = new Date();
+                var currDayOfMonth = currDate.getDate();
+                if (currDayOfMonth.length < 2)
+                {
+                    mailReceivedInDay = text.substr(14, 1)
+                }
+                else
+                {
+                    mailReceivedInDay = text.substr(14, 2)
+                }
+
+                logger.info(mailReceivedInDay);
+
+                callback(mailReceivedInDay, "err");
+            });
+
+        };
+
+
+        exports.getLatestMessageHourAsString = function (webdriver, callback) {
+
+            var mailReceivedHour;
+
+            webdriver.findElement(By.css('#mailhaut > div:nth-child(4)')).getText().then(function (text) {
+
+                var currDate = new Date();
+                var currhour = currDate.getHours();
+                if (currhour.length < 2) {
+                    mailReceivedHour = text.substr(17, 1)
+                }
+                else {
+                    mailReceivedHour = text.substr(17, 2)
+                }
+                logger.info(mailReceivedHour);
+
+                callback(mailReceivedHour, "err");
+
+
+            });
+
+        }
+
+
+
